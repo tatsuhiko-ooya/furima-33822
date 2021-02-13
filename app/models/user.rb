438eea217a7bf2bb051a3a68231c3fb250b7dcb7
tatsuhiko_ooya <tatsuhiko.ooya.prog@gmail.com>
2021-02-13 18:40:57 +0900

    userモデルのバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true, { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '平仮名、全角カタカナ、漢字のみ使用可能です。' }
-  validates :first_name, presence: true, { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '平仮名、全角カタカナ、漢字のみ使用可能です。' }
-  validates :last_name_kana, presence: true, { with: /\A[ァ-ヶー－]+\z/, message: '平仮名、全角カタカナ、漢字のみ使用可能です。' }
-  validates :first_name_kana, presence: true, { with: /\A[ァ-ヶー－]+\z/, message: '平仮名、全角カタカナ、漢字のみ使用可能です。' }
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '平仮名、全角カタカナ、漢字のみ使用可能です。' }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '平仮名、全角カタカナ、漢字のみ使用可能です。' }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '平仮名、全角カタカナ、漢字のみ使用可能です。' }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '平仮名、全角カタカナ、漢字のみ使用可能です。' }
   validates :birth_day, presence: true
 
 end
