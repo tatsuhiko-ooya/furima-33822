fb01a8532693569e4479eef3f42392b066fe2b69
tatsuhiko_ooya <tatsuhiko.ooya.prog@gmail.com>
2021-02-14 18:10:28 +0900

    バリデーションをwith_optionsを使用するように変更した

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '平仮名、全角カタカナ、漢字のみ使用可能です。' }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '平仮名、全角カタカナ、漢字のみ使用可能です。' }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみ使用可能です。' }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみ使用可能です。' }
-  validates :birth_day, presence: true
-
+  with_options presence: true do
+    validates :nickname
+    validates :birth_day
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '平仮名、全角カタカナ、漢字のみ使用可能です。' } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナのみ使用可能です。' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 end
