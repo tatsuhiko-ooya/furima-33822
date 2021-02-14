3fd06c065ead608b757f2621e8754c6688ea163a
tatsuhiko_ooya <tatsuhiko.ooya.prog@gmail.com>
2021-02-14 13:07:54 +0900

    新規登録失敗後にブラウザ再読み込みをするとエラーが出るのを修正した

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get '/users', to: 'devise/registrations#new'
+    get '/users/password', to: 'devise/passwords#new'
+  end
+  
   root to: 'products#index'
 end
