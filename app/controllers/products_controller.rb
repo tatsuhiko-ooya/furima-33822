1e9cb45f95b9cf866fca1ac7f065ca36d8aa6f38
tatsuhiko_ooya <tatsuhiko.ooya.prog@gmail.com>
2021-02-16 20:19:22 +0900

    商品の出品の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,16 +3,20 @@ class ProductsController < ApplicationController
   end
 
   def new
-
+    @product = Product.new
   end
 
   def create
-
+    product = Product.new(product_params)
+    if product.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   private
   def product_params
-    params.require(:product).permit(:name, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :days_id, :price, :image)
-    .merge(user_id: current_user.id)
+    params.require(:product).permit(:name, :description, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :days_id, :price, :image).merge(user_id: current_user.id)
   end
 end
