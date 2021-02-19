92ba56ab08e4eb5966a00009bd79e89fa3c01cbc
tatsuhiko_ooya <tatsuhiko.ooya.prog@gmail.com>
2021-02-19 19:05:49 +0900

    editアクションの実装、ビューファイルの編集途中

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,6 +18,10 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
   def show
     @product = Product.find(params[:id])
   end
