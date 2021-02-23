7032c581e9eecf71a0a94311f93c8825da3cc767
tatsuhiko_ooya <tatsuhiko.ooya.prog@gmail.com>
2021-02-23 11:07:31 +0900

    不要なコメントアウトを削除

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,13 +3,6 @@ class Address < ApplicationRecord
   belongs_to :prefecture
   belongs_to :order
 
-  # with_options presence: true do
-  #   validates :post_code
-  #   validates :prefecture_id, numericality: {other_than: 1}
-  #   validates :city
-  #   validates :phone_number
-  #   validates :block
-  # end
 end
 
 
