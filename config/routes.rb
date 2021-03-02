f50455dbd76132bdb43eabe59983ff583b288a51
tatsuhiko_ooya <tatsuhiko.ooya.prog@gmail.com>
2021-03-03 00:01:51 +0900

    詳細ページのレイアウト実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
 
   #エラー防止のために記述しています。
@@ -11,4 +12,6 @@ Rails.application.routes.draw do
   resources :products do
     resources :orders, only: [:create, :index]
   end
+
+  resources :users, only: [:show]
 end
