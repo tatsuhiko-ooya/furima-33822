2d4aab2b5890a2ae950fca54e414d5dbd0967a79
tatsuhiko_ooya <tatsuhiko.ooya.prog@gmail.com>
2021-03-08 17:55:05 +0900

    herokuのエラーへの対応

--- spec/support/auth_helper.rb
+++
@@ -1,7 +0,0 @@
-module AuthHelper
-  def http_login
-    user = ENV['BASIC_AUTH_USER']
-    pw = ENV['BASIC_AUTH_PASSWORD']
-    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user,pw)
-  end
-end
